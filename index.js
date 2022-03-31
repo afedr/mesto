54513bb622cc6f3257af73feae7f433043adcc55
AFedorova <anfed722@gmail.com>
2022-03-31 20:02:50 +0300

    hotfix:js form

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const editProfile = document.querySelector('.profile__edit-button');
 const modalWindow = document.querySelector('.popup');
 const modalCloseWindow = modalWindow.querySelector('.popup__close');
-
-function toggleModalWindow() {
-  modalWindow.classList.toggle('popup_is-active');
-}
-
-editProfile.addEventListener('click', toggleModalWindow);
-
-modalCloseWindow.addEventListener('click', toggleModalWindow);
-
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_about');
 let nameEnter = document.querySelector('.profile__name');
 let jobEnter = document.querySelector('.profile__about');
 
+
+function openModalWindow() {
+  modalWindow.classList.toggle('popup_is-active');
+  nameInput.value = nameEnter.textContent;
+  jobInput.value = jobEnter.textContent;
+}
+
+function closeModalWindow() {
+  modalWindow.classList.toggle('popup_is-active');
+}
+
 function formSubmitHandler(evt) {
-
   evt.preventDefault();
-
 
   nameEnter.textContent = nameInput.value;
   jobEnter.textContent = jobInput.value;
 
-  toggleModalWindow();
+  closeModalWindow();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+editProfile.addEventListener('click', openModalWindow);
+modalCloseWindow.addEventListener('click', closeModalWindow);
