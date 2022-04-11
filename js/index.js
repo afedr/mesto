adbf4e7da4afc609f4e360e188b69a50cb5fa59c
AFedorova <anfed722@gmail.com>
2022-04-11 12:06:34 +0400

    js added

--- js/index.js
+++ js/index.js
@@ -36,56 +36,10 @@ let jobEnter = document.querySelector('.profile__about');
 const cardsContainer = document.querySelector('.elements__container');
 const template = document.querySelector('.template-card');
 
-// visualization of cards on the page
-function render() {
-  const htmlCards = initialCards.map(createElement);
-  cardsContainer.append(...htmlCards);
-}
-
-function createElement(item) {
-  const getElementTemplate = template.content.cloneNode(true);
-  const title = getElementTemplate.querySelector('.elements__title');
-  const link = getElementTemplate.querySelector('.elements__photo')
-  title.textContent = item.name;
-  link.src = item.link;
-
-
-  getElementTemplate.querySelector('.elements__like-button').addEventListener('click', function(evt) {
-    evt.target.classList.toggle('elements__like-button_active');
-  });
-  return getElementTemplate;
-
-};
-
-render();
-
-
-//edit profile
-function openModalWindow() {
-  modalWindow.classList.toggle('popup_is-active');
-  nameInput.value = nameEnter.textContent;
-  jobInput.value = jobEnter.textContent;
-} // Открытие popup и сохрание данных из профиля
-
-function closeModalWindow() {
-  modalWindow.classList.toggle('popup_is-active');
-} // Закрытие popup
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-
-  nameEnter.textContent = nameInput.value;
-  jobEnter.textContent = jobInput.value;
-
-  closeModalWindow();
-} // Сохранение введенных данных в профиль
-
-formElement.addEventListener('submit', formSubmitHandler);
-editProfile.addEventListener('click', openModalWindow);
-modalCloseWindow.addEventListener('click', closeModalWindow);
-
-//add cards
-
+const pictureWindow = document.querySelector('#popup-pic');
+const pictureImage = document.querySelector('.popup__img');
+const pictureTitle = document.querySelector('.popup__title-img');
+const closePicWindowButton = document.querySelector('.popup__close-img');
 
 const popupAddCard = document.querySelector('#popup-add');
 const toggleFormButton = document.querySelector('.profile__button');
@@ -94,6 +48,67 @@ const closeCardButton = document.querySelector('.popup__close_submit-card');
 let newCardTitle = document.querySelector('.popup__input_type_title');
 let newCardLink = document.querySelector('.popup__input_type_link');
 
+
+
+function createElement(item) {
+  const getElementTemplate = template.content.cloneNode(true);
+  const title = getElementTemplate.querySelector('.elements__title');
+  const link = getElementTemplate.querySelector('.elements__photo')
+  const deleteButton = getElementTemplate.querySelector('.elements__delete');
+
+  title.textContent = item.name;
+  link.src = item.link;
+
+  // add like button
+  getElementTemplate.querySelector('.elements__like-button').addEventListener('click', function(evt) {
+    evt.target.classList.toggle('elements__like-button_active');
+  });
+
+  // delete card
+  deleteButton.addEventListener('click', function() {
+    const listItem = deleteButton.closest('.elements__card');
+    listItem.remove();
+  });
+
+  // open image
+  link.addEventListener('click', function() {
+    pictureWindow.classList.toggle('popup_is-active');
+    pictureImage.src = link.src;
+    pictureTitle.textContent = title.textContent;
+  });
+
+  return getElementTemplate;
+};
+
+// visualization of cards on the page
+function render() {
+  const htmlCards = initialCards.map(createElement);
+  cardsContainer.append(...htmlCards);
+}
+
+render();
+
+//edit profile
+function openModalWindow() {
+  modalWindow.classList.toggle('popup_is-active');
+  nameInput.value = nameEnter.textContent;
+  jobInput.value = jobEnter.textContent;
+}
+
+function closeModalWindow() {
+  modalWindow.classList.toggle('popup_is-active');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  nameEnter.textContent = nameInput.value;
+  jobEnter.textContent = jobInput.value;
+
+  closeModalWindow();
+}
+
+//add cards
 function openAddCardWindow() {
   popupAddCard.classList.toggle('popup_is-active');
   newCardTitle.value = "";
@@ -117,16 +132,17 @@ function cardSubmitHandler(evt) {
   closeAddCardWindow();
 }
 
+//close image window
+function closePicWindow() {
+  pictureWindow.classList.toggle('popup_is-active');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+editProfile.addEventListener('click', openModalWindow);
+modalCloseWindow.addEventListener('click', closeModalWindow);
+
 submitCardForm.addEventListener('submit', cardSubmitHandler);
 toggleFormButton.addEventListener('click', openAddCardWindow);
 closeCardButton.addEventListener('click', closeAddCardWindow);
 
-//like button
-
-const LikeButton = document.querySelector('.elements__like-button');
-
-function toggleLikeButton() {
-  LikeButton.classList.toggle('.elements__like-button_active');
-}
-
-LikeButton.addEventListener('click', toggleLikeButton);
+closePicWindowButton.addEventListener('click', closePicWindow);
